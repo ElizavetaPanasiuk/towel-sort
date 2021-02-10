4eb1e294c3b363bdc9dcc179bdaa1e19589e0a7a
ElizavetaPanasiuk <65709545+ElizavetaPanasiuk@users.noreply.github.com>
2021-02-10 19:27:25 +0300

    add solving

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return !Array.isArray(matrix) || !matrix.length ? [] : matrix.map((elem, index) => index % 2 === 0 ? elem : elem.reverse()).toString().split(',').map(elem => +elem) || [];
 }
